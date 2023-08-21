370b5630e9e4ee45a05d0a4d989f9b9c8683207b
will0010077 <will0010077@gmail.com>
2023-08-21 18:04:34 +0800

    finish BYOL

--- code/train_encoder.py
+++ code/train_encoder.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from dataset import trainDataset,collect_fn,trainDataset_NEWS,collect_fn_news
-from model import Roberta
+from model import Roberta, momentum_update
 import torch.nn as nn
 import torch.nn.functional as F
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -12,55 +12,58 @@ lam=25
 mu=25
 nu=1
 
+update_step=1
 
-def trainer(i):
-    loss_list=[]
-    model.train()
+
+def trainer(epoch, model_on:nn.Module, model_off:nn.Module):
+    losses = 0
+    model_on.train()
+    model_off.eval()
+    counter=0
     for input_ids,input_masks ,labels,_ in (bar:=tqdm(train_dataloader,ncols=0)):
+        counter+=1
         input_ids=input_ids.to(device)
         labels=labels.to(device)
         input_masks=input_masks.to(device)
 
         optimizer.zero_grad()
-        _,z_a  = model(input_ids,input_masks)
-        _,z_b =model(input_ids,input_masks)
-        sim_loss = mse_loss(z_a, z_b)
-        # variance loss
-        std_z_a = torch.sqrt(z_a.var(dim=0) + 1e-04)
-        std_z_b = torch.sqrt(z_b.var(dim=0) + 1e-04)
-        std_loss = torch.mean(F.relu(1 - std_z_a)) + torch.mean(F.relu(1 - std_z_b))
-        # covariance loss
-        z_a = z_a - z_a.mean(dim=0)
-        z_b = z_b - z_b.mean(dim=0)
-        N=input_ids.shape[0]
-        D=z_a.shape[1]
-        cov_z_a = (z_a.T @ z_a) / (N - 1)
-        cov_z_b = (z_b.T @ z_b) / (N - 1)
-        cov_loss = torch.masked_select(cov_z_a, (1-torch.eye(cov_z_a.shape[0],device=device)).to(torch.bool)).pow_(2).sum() / D
-        + torch.masked_select(cov_z_b, (1-torch.eye(cov_z_b.shape[0],device=device)).to(torch.bool)).pow_(2).sum() / D
-        # loss
-        loss = lam * sim_loss + mu * std_loss + nu * cov_loss
+        z_on, q_on = model_on(input_ids,input_masks)
+        with torch.no_grad():
+            z_off, q_off = model_off(input_ids,input_masks)
+        del z_on, q_off     #delete online latent and offline prediction
+
+
+        # MSE of online prediction and offline latent
+        loss = torch.mean(( q_on - z_off )**2)
 
         loss.backward()
         optimizer.step()
-        loss_list.append(loss.item())
-        # print(labels.shape)
-        acc=0#torch.sum(torch.argmax(predict,dim=-1)==labels)/N
-        
+
+        if counter%update_step==0:
+            momentum_update(model_on, model_off)
+
+        # log
+        if losses==0:
+            losses = loss.item()
+        losses=0.98*losses+0.02*loss.item()
         bar.set_description(f'epoch[{i+1:3d}/{num_epochs}]|Training')
-        bar.set_postfix_str(f'loss {sum(loss_list)/len(loss_list):.4f} acc {acc:.4f}')
+        bar.set_postfix_str(f'loss {losses:.4f}')
     lr_scher.step()
 
 if __name__=='__main__':
     dataset=trainDataset_NEWS()
-    
+
     train_dataloader = DataLoader(dataset, batch_size=32, shuffle=True,collate_fn=collect_fn_news)
-    model=Roberta()
-    model.to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-6/(256/32)**0.5,weight_decay=1e-6) 
+    model_on=Roberta()
+    model_off=Roberta()
+    model_on.to(device)
+    model_off.to(device)
+
+    momentum_update(model_on, model_off, 1) #full copy online to offline
+    optimizer = torch.optim.AdamW(model_on.parameters(), lr=3e-5, weight_decay=1e-2)
     num_epochs=1000
-    lr_scher=torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, num_epochs, eta_min=1e-8/(256/32)**0.5, )
-    
+    lr_scher=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=10, cooldown=20, min_lr=1e-6)
+
     for i in range(num_epochs):
         trainer(i)
-    torch.save(model.state_dict(),'save/save.pt')
+    torch.save(model_on.state_dict(),'save/save.pt')
