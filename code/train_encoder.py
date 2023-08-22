fee7a14facbeeb1fc4311575a29c05d23fec89a2
will0010077 <will0010077@gmail.com>
2023-08-22 12:41:44 +0800

    start BYOL training

--- code/train_encoder.py
+++ code/train_encoder.py
@@ -19,23 +19,36 @@ def trainer(epoch, model_on:nn.Module, model_off:nn.Module):
     model_off.eval()
     counter=0
     for ids_a, ids_b, input_masks in (bar:=tqdm(train_dataloader,ncols=0)):
+        loss=0
+        optimizer.zero_grad()
         counter+=1
         ids_a=ids_a.to(device)
         ids_b=ids_b.to(device)
         input_masks=input_masks.to(device)
 
-        optimizer.zero_grad()
+
         z_on, q_on = model_on(ids_a,input_masks)
         with torch.no_grad():
             z_off, q_off = model_off(ids_b,input_masks)
         del z_on, q_off     #delete online latent and offline prediction
 
-
+        loss += 2-2*((q_on*z_off).sum(dim=1)/(torch.norm(q_on, dim=1)*torch.norm(z_off, dim=1))).mean()
         # MSE of online prediction and offline latent
-        q_on=F.normalize(q_on)
-        z_off=F.normalize(z_off)
-        print(q_on , z_off )
-        loss = torch.mean(( q_on - z_off )**2)
+        # q_on=F.normalize(q_on)
+        # z_off=F.normalize(z_off)
+        # loss = torch.mean(( q_on - z_off )**2)
+
+        z_on, q_on = model_on(ids_b,input_masks)
+        with torch.no_grad():
+            z_off, q_off = model_off(ids_a,input_masks)
+        del z_on, q_off     #delete online latent and offline prediction
+
+        loss += 2-2*((q_on*z_off).sum(dim=1)/(torch.norm(q_on, dim=1)*torch.norm(z_off, dim=1))).mean()
+        # MSE of online prediction and offline latent
+        # q_on=F.normalize(q_on)
+        # z_off=F.normalize(z_off)
+        # loss += torch.mean(( q_on - z_off )**2)
+
 
         loss.backward()
         optimizer.step()
@@ -60,7 +73,7 @@ if __name__=='__main__':
     model_on.to(device)
     model_off.to(device)
 
-    momentum_update(model_on, model_off, 1) #full copy online to offline
+    # momentum_update(model_on, model_off, 1) #full copy online to offline
     optimizer = torch.optim.AdamW(model_on.parameters(), lr=3e-5, weight_decay=1e-2)
     num_epochs=1000
     lr_scher=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=10, cooldown=20, min_lr=1e-6)
